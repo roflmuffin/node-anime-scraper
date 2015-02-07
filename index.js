aede909104b9925c2e48c562983ecc79ac254472
Tom Jordell <tom@rabaldermedia.se>
2015-02-07 19:38:43 +0100

    Upgraded documentation a notch and re-constructed some classes

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ var async = require('async')
 var WEBSITE_ROOT = 'http://kissanime.com'
 
 var AnimeUtils = {
-  /*  KissAnime uses a POST driven search that returns a table of rows  with links to anime pages.
-    Returns Promise of {name, url}.
-    NOTE: Searching with an empty name tag will return the entire list of anime.
-    */
+
+  /**
+   * Sends a POST request to KissAnime and expects a HTML response. Empty name parameter returns entire list of anime.
+   * @param  {string} name
+   * @return {promise}
+   */
   searchByName: function (name) {
     var deferred = Q.defer()
     var url = WEBSITE_ROOT + '/AdvanceSearch'
@@ -63,22 +65,41 @@ var PageUtils = {
   /*  These helper functions parse information about the anime on the page,
     and boy is their HTML structure ugly!
    */
+  /**
+   * Splits the foreign names into an array from a string
+   * @param  {string} text The string to be split
+   * @return {array}       The split string
+   */
   parseForeignNames: function (text) {
     text = text.replace(/Other name:/, '').trim()
     return text.split(';')
   },
 
+  /**
+   * Splits genres into an array from a string
+   * @param  {string} text The string to be split
+   * @return {array}       The split string
+   */
   parseGenres: function (text) {
     text = text.replace(/Genres:/, '').trim()
     return text.split(', ')
   },
 
+  /**
+   * Splits the date it was added into an array from a string
+   * @param  {string} text The string to be split
+   * @return {array}       The split string
+   */
   parseDateAired: function (text) {
     text = text.replace(/Date aired:/, '').trim()
     return text.split(' to ')
   },
 
-  /* Video links are held in a select tag dropdown. */
+  /**
+   * Iterates over a set of option elements to get video quality links
+   * @param  {object} $ The cheerio object holding the elements
+   * @return {array}    Holds the quality names and links
+   */
   getQualityLinkList: function ($) {
     var arr = []
     $('#selectQuality > option').each(function (index) {
@@ -90,8 +111,11 @@ var PageUtils = {
     return arr
   },
 
-  /* Returns row elements in main content table
-  * Used by search & episode retrieval */
+  /**
+   * Returns row elements in main content table
+   * @param  {object} $ The cheerio object holding the elements
+   * @return {array}    The row elements
+   */
   getPageRows: function ($) {
     var elements = $('.listing tr:not(.head):not([style])').toArray()
     var rows = []
@@ -106,7 +130,11 @@ var PageUtils = {
     return rows
   },
 
-  /* Shortcut method to just get name & href from content table */
+  /**
+   * Shortcut to only get the name & href from the table
+   * @param  {object} $ The cheerio object holding the elements
+   * @return {array}    Holding the name & link of each element
+   */
   getNameLinkRows: function ($) {
     var rows = this.getPageRows($)
     var arr = []
@@ -126,54 +154,72 @@ var PageUtils = {
   }
 }
 
-/* Mini class definition for episode objects
- *  Lets you call getVideoUrl on each individual object to get their videoUrl information */
+/**
+ * Episode constructor
+ * @param {string} name    The name of the episode
+ * @param {string} pageUrl URL to the page
+ */
 var Episode = function (name, pageUrl) {
   this.name = name
   this.pageUrl = pageUrl
+}
 
-  this.getVideoUrl = function () {
-    var deferred = Q.defer()
-    var self = this
+/**
+ * Fetches videoUrl information of an episode
+ * @return {promise}
+ */
+Episode.getVideoUrl = function () {
+  var deferred = Q.defer()
+  var self = this
 
-    got(this.pageUrl, function (err, data, resp) {
-      if (err) deferred.reject(new Error('Page could not be loaded'))
-      var $ = cheerio.load(data)
+  got(this.pageUrl, function (err, data, resp) {
+    if (err) deferred.reject(new Error('Page could not be loaded'))
+    var $ = cheerio.load(data)
 
-      self.videoUrls = PageUtils.getQualityLinkList($)
-      deferred.resolve(self.videoUrls)
-    })
-    return deferred.promise
-  }
+    self.videoUrls = PageUtils.getQualityLinkList($)
+    deferred.resolve(self.videoUrls)
+  })
+
+  return deferred.promise
 }
 
 /* Encapsulates anime information
  *  Includes method to retrieve videoUrls for every episode */
+
+/**
+ * Anime constructor
+ * @param {string} name Name of anime
+ * @param {string} url  Link to anime
+ */
 var Anime = function (name, url) {
   this.name = name
   this.url = url
-
-  this.getVideoUrls = function () {
-    var deferred = Q.defer()
-    async.map(this.episodes, function (episode, callback) {
-      episode.getVideoUrl().then(function () {
-        callback(null, episode)
-      })
-    },
-    function (err, results) {
-      if (err) deferred.reject(new Error('Video links could not be loaded'))
-      deferred.resolve(results)
-    })
-
-    return deferred.promise
-  }
 }
 
-/* Anime Object Factory Methods */
+/**
+ * Fetches videoUrls for every episode
+ * @return {promise}
+ */
+Anime.getVideoUrls = function () {
+  var deferred = Q.defer()
+  async.map(this.episodes, function (episode, callback) {
+    episode.getVideoUrl().then(function () {
+      callback(null, episode)
+    })
+  },
+  function (err, results) {
+    if (err) deferred.reject(new Error('Video links could not be loaded'))
+    deferred.resolve(results)
+  })
 
-/*
- Searches the website using POST and then creates Anime from name.
- Note this is significantly slower than using a url directly as POST search is slow
+  return deferred.promise
+}
+
+/**
+ * Sends a POST request to the search function on the page and creates an Anime object from it.
+ * Note: Significantly slower than using a direct URL as this is an additional step to figure out the URL
+ * @param  {string} name Name of the anime to search for
+ * @return {promise}
  */
 Anime.fromName = function (name) {
   var deferred = Q.defer()
@@ -194,8 +240,10 @@ Anime.fromName = function (name) {
   return deferred.promise
 }
 
-/*  Creates an anime object from a given url.
-  This includes information like genres, other names and episode links (but not video links).
+/**
+ * Sends a GET request to a given URL and creates a Anime object from the page contents.
+ * @param  {string} pageUrl The URL to the anime page
+ * @return {promise}
  */
 Anime.fromUrl = function (pageUrl) {
   var deferred = Q.defer()
