2f01b945aa53489181ffc70b96fff1d9c27a383e
andrewprivate <32175943+andrewprivate@users.noreply.github.com>
2019-05-19 22:26:30 -0400

    Fix

--- src/html.js
+++ src/html.js
@@ -2,7 +2,7 @@ const cheerio = require('cheerio');
 const got = require('got');
 const debug = require('debug')('anime-scraper-html');
 
-const BASE_URL = 'https://ww1.gogoanime.io';
+const BASE_URL = 'https://gogoanime.io';
 
 // Search Results
 function parseSearchResults($) {
@@ -40,20 +40,62 @@ function parseAnimePage($) {
 const VideoProviders = {
   Vidstreaming(url) {
     return got(url).then(resp => cheerio.load(resp.body))
-    .then($ =>
-      $('video source').map((i, val) => ({
-        name: $(val).attr('label'),
-        url: $(val).attr('src'),
-      })).get());
+      .then($ =>
+        $('.linkserver').map((i, val) => {
+          val = $(val);
+          var source = val.attr('data-video');
+          var type = "iframe"
+          if (!source && val.hasClass("active")) {
+            $('script').each((i, el) => {
+              var html = $(el).html();
+              var ind = html.indexOf("playerInstance.setup({");
+              if (ind != -1) {
+
+                ind += "playerInstance.setup({".length;
+                var curly = 1;
+                var text = false;
+                var results = [];
+                var str = "";
+                for (; ind < html.length; ind++) {
+                  var char = html.charAt(ind);
+
+                  if (char == "'") {
+                    text = !text
+                    if (!text) {
+                      results.push(str)
+                      str = "";
+                    }
+                  } else if (!text && char == "{") {
+                    curly++;
+                  } else if (!text && char == "}") {
+                    curly--;
+                    if (curly == 0) break;
+                  } else if (text) {
+                    str += char
+                  }
+
+                }
+                source = results[0]
+                type = "source"
+              }
+            })
+          }
+          return {
+            name: val.text(),
+            url: source,
+            type: type
+          }
+        }).get());
   },
 };
 
 function parseVideo($) {
-  const vidStreaming = $('[data-video*="https://vidstreaming.io/"]').attr('data-video');
+
+  const vidStreaming = $('[data-video*="//vidstreaming.io/"]').attr('data-video');
   debug(`Found Vidstreaming link: ${vidStreaming}`);
 
   if (vidStreaming != null) {
-    return VideoProviders.Vidstreaming(vidStreaming);
+    return VideoProviders.Vidstreaming("http:" + vidStreaming);
   }
 
   return null;
